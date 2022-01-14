1723dfab6cbbe2e6ab21b6c14bfd9012fe492763
RedByte1993 <michaelsebzenberger93@gmail.com>
2022-01-14 13:42:05 +0100

    20220114 | add class Vector & add class PolarCoordinates

---
+++ AnalogClock/Classes/PolarCoordinates.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace AnalogClock.Classes
+{
+    public class PolarCoordinates
+    {
+        #region Properties
+        public double Radius { get; set; }
+        public double Angle { get; set; }
+        #endregion
+        #region Ctor
+        public PolarCoordinates(double radius, double angle)
+        {
+            Radius = radius;
+            Angle = angle;
+        }
+        #endregion
+        #region Methodes
+        public Vector ConvertToVector()
+        {
+            return new Vector(Math.Round(Radius * Math.Cos(Angle), 14), Math.Round(Radius * Math.Sin(Angle), 14));
+        }
+        #endregion
+    }
+}
---
+++ AnalogClock/Classes/Vector.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Drawing;
+namespace AnalogClock.Classes
+{
+    public class Vector
+    {
+        #region Properties
+        public double X { get; set; }
+        public double Y { get; set; }
+        #endregion
+        #region Ctor
+        public Vector(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+        #endregion
+        #region Methodes
+        public double ScalarProduct(Vector other)
+        {
+            return X * other.X + Y * other.Y;
+        }
+        public double Magnitude()
+        {
+            return Math.Sqrt(X * X + Y * Y);
+        }
+        public PolarCoordinates ConvertToPolarCoordinates()
+        {
+            return new PolarCoordinates(Magnitude(), Math.Atan(Y / X));
+        }
+        public PointF CalculatePoint(PointF p)
+        {
+            return new PointF((float)(p.X + X), (float)(p.Y + Y));
+        }
+        #endregion
+    }
+}
