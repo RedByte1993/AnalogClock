aaa22f638ba9cad2bb2663d2eb3db3f30e22037e
RedByte1993 <michaelsebzenberger93@gmail.com>
2022-01-14 13:45:41 +0100

    20220114 | add class AngleConverter

---
+++ AnalogClock/Classes/AngleConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalogClock.Classes
+{
+    public static class AngleConverter
+    {
+        public static double ConvertToRadians(double degree)
+        {
+            return degree * Math.PI / 180.0;
+        }
+        public static double ConvertToDegree(double radians)
+        {
+            return radians * 180.0 / Math.PI;
+        }
+    }
+}
